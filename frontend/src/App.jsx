9c33abc66ef6ef658086c058e6591df7391eca65
MarzooqN <Marzooq.nadeem@gmail.com>
2024-11-02 06:57:30 -0400

    Fixed authentication issues and completed logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import Auth from './components/Auth';
-import LandingPage from './pages/LandingPage';
 import GraphPage from './pages/GraphPage';
 import SearchPage from './pages/SearchPage';
 import NavBar from './components/NavBar';
 import ProfilePage from './pages/ProfilePage';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { AuthProvider } from './supabaseContext';
 
 
 
 const App = () => {
   return (
-  <Router>
-    <div>
-   <NavBar />
-    <Routes>
-        <Route path = "/auth" element = {<Auth/>} />
-        <Route path = "/data" element = {<GraphPage/>} />
-        <Route path = "/" element = {<SearchPage/>} />
-        <Route path = "/profile" element = {<ProfilePage/>} />
-    </Routes>
-    </div>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <div>
+    <NavBar />
+      <Routes>
+          <Route path = "/auth" element = {<Auth/>} />
+          <Route path = "/data" element = {<GraphPage/>} />
+          <Route path = "/" element = {<SearchPage/>} />
+          <Route path = "/profile" element = {<ProfilePage/>} />
+      </Routes>
+      </div>
+    </Router>
+  </AuthProvider>
   
    )
   };
--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import NavBar from '../components/NavBar';
+import { useAuth } from '../supabaseContext';
+import { Navigate } from 'react-router-dom';
 
 const SearchPage = () => {
+  const { session } = useAuth();
+
+  if (!session) {
+    return <Navigate to="/Auth" replace />;
+  }
+
   return (
     <div className="min-h-screen w-full bg-gray-100 p-8 flex flex-col gap-6">
             <div className="flex flex-col items-center gap-4 mt-6">
